b5cb2883ccbba1ee9b1fdd3f0f6c8fef116820cc
Muhammad Huzaifa <huzaifakhawar100@gmail.com>
2025-06-14 22:01:52 +0500

    update URL LIVE PATH

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 // const API_URL_LOCAL = 'http://localhost:3000/api';
-const API_URL_LIVE = 'backend-pms-five.vercel.app'
+const API_URL_LIVE = 'backend-pms-five.vercel.app/api'
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: API_URL_LIVE,
