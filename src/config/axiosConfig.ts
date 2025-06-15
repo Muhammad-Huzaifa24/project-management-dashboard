6665769d4090d9aabd491e3d69da36bcea82d14f
Muhammad Huzaifa <huzaifakhawar100@gmail.com>
2025-06-15 12:54:55 +0500

    add backend updated UR;

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 // const API_URL_LOCAL = 'http://localhost:3000/api';
-const API_URL_LIVE = 'https://backend-pms-five.vercel.app/api'
+const API_URL_LIVE = 'https://pms-backend.up.railway.app/api'
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: API_URL_LIVE,
