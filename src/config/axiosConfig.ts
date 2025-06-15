63245c87250e034e13285a85e4a27751d162dbcf
Muhammad Huzaifa <huzaifakhawar100@gmail.com>
2025-06-15 14:08:44 +0500

    change url path

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -1,13 +1,13 @@
 import axios, { AxiosInstance } from 'axios';
 
-// const API_URL_LOCAL = 'http://localhost:3000/api';
-const API_URL_LIVE = 'https://pms-backend.up.railway.app/api'
+const API_URL_LOCAL = 'http://localhost:3000/api';
+// const API_URL_LIVE = 'https://pms-backend.up.railway.app/api'
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: API_URL_LIVE,
+  baseURL: API_URL_LOCAL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-export { API_URL_LIVE, axiosInstance };
+export { API_URL_LOCAL, axiosInstance };
